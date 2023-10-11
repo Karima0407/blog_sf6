1c8de557913c4596f6f723c04b16c1febc26d016
KARIMA <karima.jamazi@myyahoo.com>
2023-10-11 16:32:41 +0200

    Affichage article

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles=$articleRepository->findAll();
+        // dd($articles);
         return $this->render('home/home.html.twig', [
             'controller_name' => 'Hello World!',
+            'articles' => $articles,
         ]);
     }
+
+
+    #[Route('/show {id}', name: 'show')]
+    public function show( ArticleRepository $articleRepository ,$id): Response
+    {
+        // $articles = $articleRepository1->find();
+        // // dd($articles);
+        // return $this->render('home/home.html.twig', [
+        //     'controller_name' => 'Hello World!',
+        //     'articles' => $articles,
+        // ]);
+
+
+        $article = $articleRepository->find($id);
+        // dd($articles);
+        return $this->render('home/show.html.twig', [
+            
+            'article' => $article,
+        ]);
+    
+        
+    }
+
+    
 }
